1c10599c7dd4e4c3eac0fe0b65ac7fa1855808c2
Kartik Verma <vermakartik667@gmail.com>
2025-08-16 15:32:47 +0530

    used openrouter free key instead, now chat working fine

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,28 +1,37 @@
 import OpenAI from "openai";
 
 const openai=new OpenAI({
+    baseURL: 'https://openrouter.ai/api/v1',
     apiKey:process.env.OPENAI_API_KEY
 })
 
 export async function POST(request){
     try {
-        const {message}=request.json()
+        const {message}=await request.json()
+        if(!message || message.trim().length ==0){
+            return Response.json({
+                msg:"Message cant be empty"
+            },{status:402})
+        }
 
        const completion= await openai.chat.completions.create({
-            model:"gpt-3.5-turbo",
+            model:"gpt-4o-mini",
             messages:[
                 {role:"user",
-                    content:message
+                content:message
                 }
             ]
         })
 
         return Response.json({
             response:completion.choices[0].message.content
-        })
+        },{status:200})
         
     } catch (error) {
         console.log("some error occured",error);
+        return Response.json({
+            error:"Some error occured"
+        },{status:500})
         
         
     }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 "use client"
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 function page() {
+  const [loading, setloading] = useState(false)
+  const [message, setmessage] = useState("")
+  const [response, setresponse] = useState("hey wassup guys")
 
   async function getdata(){
     let res1=await fetch("/api/test")
@@ -33,17 +36,64 @@ function page() {
     }
   }
 
+  const handleChat= async ()=>{
+    setloading(true)
+    setresponse("")
+    // setmessage("") //reset the old msg
+
+    try {
+      const res=await fetch("/api/chat",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({message})
+      })
+
+      const data=await res.json()
+      if(data) setresponse(data.response)
+
+      return
+
+      
+    } catch (error) {
+      console.log(error,"error occured");
+      setresponse("Error "+error.message)
+      
+      
+    }finally{
+      setloading(false)
+    }
+
+  }
+
   return (
     <div>
-      <h1>hey wassup guys </h1>
-      <h1 className='text-4xl white ml-4 mt-4'>hiheheh</h1>
       <Link href="/route1">
       <button className='bg-white text-black cursor-pointer m-2 p-2 rounded-md '>Route 1</button>
 
       
       </Link>
       <button onClick={getdata} className='bg-white text-black cursor-pointer m-2 p-2 rounded-md '>Fetch data</button>
-      <button onClick={testapi_func} className='bg-white text-black cursor-pointer m-2 p-2 rounded-md '>Test API</button>
+     
+
+      <div>
+        <label htmlFor="t1">Enter message</label>
+        <br />
+        <textarea rows={5} cols={20} placeholder='enter your message here ' name="" id="t1" value={message} onChange={(e)=> setmessage(e.target.value)} className='bg-slate-800'></textarea>
+        <br />
+         <button onClick={handleChat} className='bg-white text-black cursor-pointer m-2 p-2 rounded-md '>Chat now </button>
+      </div>
+
+      {loading && (
+        <h1>Loading...</h1>
+      )}
+
+      {response && (
+        <div className='border-red-400'>
+          <h1 className='text-4xl p-3 border-1 min-w-3xs'>{response}</h1>
+        </div>
+      )}
       
     </div>
   )
