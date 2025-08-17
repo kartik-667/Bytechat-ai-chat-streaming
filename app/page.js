ab33bb86ea22db7975c5dd7b4322595365973d22
Kartik Verma <vermakartik667@gmail.com>
2025-08-17 13:22:18 +0530

    added reset button

--- app/page.js
+++ app/page.js
@@ -111,6 +111,13 @@ function Page() {
 
   }
 
+  function handleReset(){
+    setstreamdata("")
+    setmessage("")
+    setresponse("Send a message to show result")
+    
+  }
+
 
 
 
@@ -163,6 +170,15 @@ function Page() {
                 className="inline-flex items-center justify-center rounded-xl bg-indigo-600 px-5 py-2.5 text-sm font-medium shadow-md hover:bg-indigo-500 transition focus:outline-none focus:ring-2 focus:ring-indigo-500"
               >
                 Stream Response
+              </button>
+              <button
+                onClick={() => {
+                  handleReset();
+                  
+                }}
+                className="inline-flex items-center justify-center rounded-xl bg-red-600 px-5 py-2.5 text-sm font-medium shadow-md hover:bg-red-500 transition focus:outline-none focus:ring-2 focus:ring-red-500"
+              >
+                Reset chat
               </button>
 
               {loading && (
